a1ce687143af07c3e8abefc6556b9f90c440bbd9
Guillermo Galan <guille@clutchapp.io>
2023-06-19 23:19:35 +0200

    Rename app

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule SlipstreamClient.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Client.Application, []}
+      mod: {SlipstreamClient.Application, []}
     ]
   end
 
